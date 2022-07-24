c562850179a15e5b296c7d534f354876d5353d53
Vandreh <esmeraldovandreh@gmail.com>
2022-07-24 03:58:26 -0300

    making adaptations for production

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { resolve } = require('path');
+const app = express();
+
+app.use('/', express.static(resolve(__dirname, './build')));
+
+app.listen(process.env.PORT, (err) => {
+    if (err) { return console.log(err)
+    } else { console.log('Everything ok')}
+});
